be372051035e441c71ea2fccb053e31a643254d9
Ritav <ritav08092004@gmail.com>
2026-04-23 06:17:11 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.04.2026 18:00:48
-// Design Name: 
-// Module Name: Dff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Dff(
    input D_i, 
