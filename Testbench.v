d6b39a320952e27d9900bac46debc53530a15a6d
Ritav <ritav08092004@gmail.com>
2026-04-23 06:17:43 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 20.04.2026 18:01:16
-// Design Name: 
-// Module Name: tb_Dff
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_Dff();
 reg D_ti; 
